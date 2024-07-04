fab0ed88d6d519efc7ef28515562a50b8988cda4
123 <icekingmoore@gmail.com>
2024-07-04 10:42:32 +0800

    新增mess APP

--- guestbook/settings.py
+++ guestbook/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mess',
 ]
 
 ROOT_URLCONF = 'guestbook.urls'
